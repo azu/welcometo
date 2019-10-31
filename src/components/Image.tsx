4cbb58015b58979d9b26ae94bd9cf34bd60d7ef3
azu <azuciao@gmail.com>
2019-11-01 00:24:37 +0900

    fix: terminal.app fallback

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import { Box } from "ink";
 
+
 const termImg = require("term-img");
 const terminalImage = require("terminal-image");
-
 export type ImageProps = {
     src: string;
     width: string;
     height: string;
 }
 
-export const Image = (props: ImageProps) => (
-    <Box paddingLeft={25}>
-        {termImg.string(props.src, {
-            width: props.width,
-            height: props.height,
-            fallback: async () => {
-                return await terminalImage.file(props.src);
-            }
-        })}
-    </Box>
-);
+export class Image extends React.Component<ImageProps, { image: string }> {
+    constructor(props: ImageProps) {
+        super(props);
+        this.state = {
+            image: ""
+        };
+    }
+
+    componentDidMount() {
+        const fallback = () => {
+            terminalImage.buffer(this.props.src).then((result: string) => {
+                console.log(result);
+            });
+            return "";
+        };
+        const result = termImg.string(this.props.src, {
+            width: this.props.width,
+            height: this.props.height,
+            fallback
+        });
+        if (result) {
+            this.setState({
+                image: result
+            });
+        }
+    }
+
+    render() {
+        return <Box>{this.state.image}</Box>;
+    }
+}
