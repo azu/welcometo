e522d4fa0bfe8b39cc3709b6b299484c97f17fa0
azu <azuciao@gmail.com>
2019-11-01 00:29:36 +0900

    fix

--- src/components/AsciiArt.tsx
+++ src/components/AsciiArt.tsx
@@ -5,9 +5,9 @@ const BigText = require("ink-big-text");
 
 export type AsciiArtProps = {
     text: string;
-}
+};
 export const AsciiArt = (props: AsciiArtProps) => {
     return <Gradient name="rainbow">
-        <BigText text={props.text}/>
+        <BigText text={props.text} />
     </Gradient>;
 };
